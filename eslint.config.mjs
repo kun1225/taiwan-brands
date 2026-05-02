12ce9cdec804cb470b4302c4e353354c5f7c4bb8
kun1225 <kun881225@gmail.com>
2026-05-02 21:38:04 +0800

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,44 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Side effect imports.
+            ["^\\u0000"],
+            // Node.js built-ins.
+            ["^node:"],
+            // External packages.
+            ["^react$", "^react-dom$", "^@?\\w"],
+            // Monorepo Internal aliases.
+            ["^@repo(/.*|$)"],
+            // Internal aliases.
+            ["^@/"],
+            // Parent imports.
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // Same-folder imports.
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // Style imports.
+            ["^.+\\.(css)$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
